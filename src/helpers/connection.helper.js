39b69a5755e18b6fd827be06be6c425915756fb3
Iván Cortés <69217213+icortes99@users.noreply.github.com>
2022-07-03 19:41:30 -0600

    update

--- src/helpers/connection.helper.js
+++ src/helpers/connection.helper.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('thirdproject', 'bd238f0529ff8d', 'c5dcee8d', {
+const sequelize = new Sequelize('heroku_1ec469e8a250f51', 'bd238f0529ff8d', 'c5dcee8d', {
     dialect: 'mysql',
     host: 'us-cdbr-east-06.cleardb.net'
 })
